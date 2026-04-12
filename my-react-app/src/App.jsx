8a36a8c5f58f24f2da552803cf73aa3f9663eafa
Kripamoye <biswaskripamoye@gmail.com>
2026-04-11 20:16:38 -0400

    updated page routes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/landing'
 import Navbar from './pages/navbar'
@@ -6,23 +6,25 @@ import HomePage from './pages/home'
 import ProfilePage from './pages/profile'
 import DestinationPage from './pages/destination'
 
+function AppLayout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
+}
+
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/app"
-        element={
-          <>
-            <Navbar />
-            <Routes>
-              <Route index element={<HomePage />} />
-              <Route path="home" element={<HomePage />} />
-              <Route path="destination" element={<DestinationPage />} />
-              <Route path="profile" element={<ProfilePage />} />
-            </Routes>
-          </>
-        }
-      />
+      <Route path="/app" element={<AppLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<HomePage />} />
+        <Route path="destination" element={<DestinationPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+      </Route>
     </Routes>
   )
 }
