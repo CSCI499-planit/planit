6605d29b5e4b416ab81361fc9079627cc3d49a84
Sifatun <sifanoor04@gmail.com>
2026-04-14 23:00:24 -0400

    Made changes to navbar

--- my-react-app/src/pages/navbar.jsx
+++ my-react-app/src/pages/navbar.jsx
@@ -1,4 +1,6 @@
 import { Link, useLocation } from 'react-router-dom'
+import { FaHome, FaSearchLocation, FaUser, FaBell, FaRegBell, FaCog, FaGlobeAmericas} from 'react-icons/fa'
+import { HiCog } from "react-icons/hi2";
 
 export default function Navbar() {
   const location = useLocation()
@@ -7,15 +9,15 @@ export default function Navbar() {
 
   return (
     <nav className="nav">
-      <Link to="/" className="nav__logo">PlanIt</Link>
+      <Link to="/" className="nav__logo"><FaGlobeAmericas /> PlanIt</Link>
       <div className="nav__links">
-        <Link to="/app/home" className={isActive('/app/home') ? 'active' : ''}>🏠 Home</Link>
-        <Link to="/app/destination" className={isActive('/app/destination') ? 'active' : ''}>🔍 Discover</Link>
-        <Link to="/app/profile" className={isActive('/app/profile') ? 'active' : ''}>👤 Profile</Link>
+        <Link to="/app/home" className={isActive('/app/home') ? 'active' : ''}> <FaHome /> Home</Link>
+        <Link to="/app/destination" className={isActive('/app/destination') ? 'active' : ''}> <FaSearchLocation /> Discover</Link>
+        <Link to="/app/profile" className={isActive('/app/profile') ? 'active' : ''}><FaUser /> Profile</Link>
       </div>
       <div className="nav__right">
-        <button className="nav__icon">🔔</button>
-        <button className="nav__icon">⚙️</button>
+        <button className="nav__icon"><FaBell /></button>
+        <button className="nav__icon"><HiCog /></button>
       </div>
     </nav>
   )
